850bcf87fda638785c7afb095c280626b8644de8
Victor Palomo <vPalomo@fuensalida.com>
2018-02-15 12:38:20 +0100

    Mejora de logs

--- lanzarProceso.bat
+++ lanzarProceso.bat
@@ -5,5 +5,8 @@ cd ..
 cd ..
 cd ..
 cd scriptFacturasFace
+echo *****************>> logs\log%date:~-4%_%date:~3,2%_%date:~0,2%.log
+date /t >> logs\log%date:~-4%_%date:~3,2%_%date:~0,2%.log
+time /t >> logs\log%date:~-4%_%date:~3,2%_%date:~0,2%.log
 node GenerarTablaFacturas.js >> logs\log%date:~-4%_%date:~3,2%_%date:~0,2%.log
 exit
